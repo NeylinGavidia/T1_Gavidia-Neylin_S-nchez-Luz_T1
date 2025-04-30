20ea33f2c9cfdccbdce2d07aaf0fa2b3e8dc23d0
Leonardo <Leonardo@upn.pe>
2025-04-30 16:41:20 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el sueldo del empleado: ");
+
+            double sueldo = double.Parse(Console.ReadLine());
+
+            double impuesto;
+
+            if (sueldo <= 1500)
+
+            {
+
+                impuesto = sueldo * 0.10;
+
+            }
+
+            else if (sueldo <= 3000)
+
+            {
+
+                impuesto = (sueldo - 1500) * 0.05;
+
+            }
+
+            else
+
+            {
+
+                impuesto = (sueldo - 3000) * 0.03;
+
+            }
+
+            double sueldoNeto = sueldo - impuesto;
+
+            Console.WriteLine();
+
+            Console.WriteLine("Resultados:");
+
+            Console.WriteLine("Impuesto retenido:  " + impuesto);
+
+            Console.WriteLine("Sueldo neto a pagar: " + sueldoNeto);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número entero positivo de 3 cifras:");
+
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero >= 100 && numero <= 999)
+
+            {
+
+                double cien = numero / 100;
+                double e1 = Math.Truncate(cien);
+                double aux1 = numero % 100;
+                double diez = aux1 / 10;
+                double aux2 = aux1 % 10;
+                double e2 = Math.Truncate(diez);
+                double aux3 = aux2 / 1;
+                double suma = e1 + e2 + aux3;
+
+                Console.WriteLine("La suma de los dígitos de " + numero + "(" + e1 + " + " + e2 + " + " + aux3 + "), es: " + suma);
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine("Error: El número debe tener 3 cifras.");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
